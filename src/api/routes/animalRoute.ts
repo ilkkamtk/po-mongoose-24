7199af139784f8f2e054b42a86fe10866a969716
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-08-27 10:50:15 +0300

    box

--- src/api/routes/animalRoute.ts
+++ src/api/routes/animalRoute.ts
@@ -5,11 +5,14 @@ import {
   getAnimal,
   postAnimal,
   putAnimal,
+  getAnimalsByBox,
 } from '../controllers/animalController';
 
 const router = express.Router();
 
 router.route('/').post(postAnimal).get(getAnimal);
+
+router.route('/location').get(getAnimalsByBox);
 
 router.route('/:id').get(getSingleAnimal).put(putAnimal).delete(deleteAnimal);
 
